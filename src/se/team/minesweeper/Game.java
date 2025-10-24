fe7a00fab1e97553e87e95d73e4ea3b2ba4558a3
Urwa6 <115958392+Urwa6@users.noreply.github.com>
2025-10-24 06:49:51 -0900

    Linked all the classes
    Linked Board & Player Class and added code for Game Over .. 
    Created a game with the specified dimensions and number of mines.

--- src/se/team/minesweeper/Game.java
+++ src/se/team/minesweeper/Game.java
@@ -1,4 +1,17 @@
 package se.team.minesweeper;
 
-public class Game {
+public class Game {   
+  //The game board
+    private final Board board;
+    //Player
+    private final Player player;
+    //True if game ends
+    private boolean gameOver;
+
+    //Creates a new game with the specified dimensions & number of mines
+    public Game(int rows, int cols, int mines) {
+        board = new Board(rows, cols, mines);
+        player = new Player();
+        gameOver = false;
+    }
 }
