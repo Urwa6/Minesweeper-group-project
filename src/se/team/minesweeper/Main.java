1cb77bf56e8f2b492cb368534ee8e6d32125eb2d
Urwa6 <115958392+Urwa6@users.noreply.github.com>
2025-10-24 06:45:56 -0900

    Created Main
    Added game function.

--- src/se/team/minesweeper/Main.java
+++ src/se/team/minesweeper/Main.java
@@ -1,4 +1,12 @@
 package se.team.minesweeper;
 
 public class Main {
+  public static void main(String[] args){
+       //Creating a new game : 8x8-board with 10 mines
+        Game game = new Game(8, 8, 10);
+
+        //Start the game
+        game.start();
+    
+  }
 }
